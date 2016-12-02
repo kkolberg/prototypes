700af89031bf5ac3b31f6791155e8a8deee32f26
kolberg <kolbergconsulting@gmail.com>
2016-12-02 13:35:01 -0500

    pull version from env

--- starter/versioning.js
+++ starter/versioning.js
@@ -3,13 +3,12 @@
 var pjson = require('./package.json');
 var mode = process.env.NODE_ENV;
 var isNotLocal = process.env.IS_NOT_LOCAL;
+var version = process.env.TAG;
 
 module.exports.version = function () {
     if (mode !== 'production') {
         return '';
     }
-
-    var version = pjson.version.split('.')[0];
 
     return '-v' + version;
 };
